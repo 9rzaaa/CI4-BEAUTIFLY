76f89a51935a28c96529917e69b36afcc029e19b
amiliadanielle <amiliadanielle>
2025-11-30 19:16:19 +0800

    feat(frg booking review): added a fragment

---
+++ backend/app/Views/components/booking_review/payment.php
@@ -0,0 +1,23 @@
+                <div class="mt-8">
+                    <h4 class="mb-2 font-bold text-primary-dark text-xl">Select Payment Method</h4>
+                    <div id="payment-logos" class="gap-3 grid grid-cols-3">
+                        <label class="payment-logo-option selected" data-value="gcash">
+                            <input type="radio" name="paymentMethod" value="gcash" checked>
+                            <img src="/assets/img/gcash.png" alt="GCash Logo">
+                            <span class="font-semibold text-primary-dark text-xs">GCash (Recommended)</span>
+                        </label>
+
+                        <label class="payment-logo-option" data-value="paymaya">
+                            <input type="radio" name="paymentMethod" value="paymaya">
+                            <img src="/assets/img/paymaya.png" alt="Maya Logo">
+                            <span class="font-semibold text-primary-dark text-xs">Maya (PayMaya)</span>
+                        </label>
+
+                        <label class="payment-logo-option" data-value="qrph">
+                            <input type="radio" name="paymentMethod" value="qrph">
+                            <img src="/assets/img/qrph.png" alt="QR Ph Logo">
+                            <span class="font-semibold text-primary-dark text-xs">QR Ph</span>
+                        </label>
+                    </div>
+                </div>
+            </div>
