d356f081c385373f55c11e301b06a652709f608d
amiliadanielle <amiliadanielle>
2025-11-30 19:16:04 +0800

    feat(frg booking review): added a fragment

---
+++ backend/app/Views/components/booking_review/button.php
@@ -0,0 +1,10 @@
+        </div>
+        <hr class="my-8 border-gray-300">
+        <div class="mt-8 text-center">
+            <button id="proceedToPayment"
+                class="bg-accent hover:bg-accent/90 shadow-xl px-12 py-3 rounded-lg font-bold text-white text-lg hover:scale-105 transition-all btn-pulse">
+                Proceed to Payment
+            </button>
+        </div>
+    </div>
+</main>
